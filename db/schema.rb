85267d4d482b0f43236bcefaa45a5d488303c33f
luis-henrique-carvalho <luisdev9225@gmail.com>
2024-05-23 01:10:09 -0300

    Add Position model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_23_035932) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_23_040922) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,22 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_23_035932) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_companies_on_user_id"
+  end
+
+  create_table "positions", force: :cascade do |t|
+    t.string "name"
+    t.integer "carrer"
+    t.integer "contract"
+    t.boolean "remote"
+    t.string "city"
+    t.string "state"
+    t.text "summary"
+    t.text "description"
+    t.boolean "publish"
+    t.bigint "company_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "index_positions_on_company_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +52,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_23_035932) do
   end
 
   add_foreign_key "companies", "users"
+  add_foreign_key "positions", "companies"
 end
