c8edac8bee3971eb8eca7d7bcee62641a5aea4b9
luis-henrique-carvalho <luisdev9225@gmail.com>
2024-05-23 01:55:37 -0300

    Add 'applicants' table to the database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_23_040922) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_23_043510) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "applicants", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone"
+    t.bigint "user_id", null: false
+    t.bigint "positions_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["positions_id"], name: "index_applicants_on_positions_id"
+    t.index ["user_id"], name: "index_applicants_on_user_id"
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -51,6 +63,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_23_040922) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "applicants", "positions", column: "positions_id"
+  add_foreign_key "applicants", "users"
   add_foreign_key "companies", "users"
   add_foreign_key "positions", "companies"
 end
