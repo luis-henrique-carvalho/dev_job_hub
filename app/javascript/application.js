01c58950337f95d5a670ad3783154b99d09806b0
luis-henrique-carvalho <luisdev9225@gmail.com>
2024-05-23 13:57:29 -0300

    Update dependencies and add AdminLTE and jQuery

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,25 @@
-// Entry point for the build script in your package.json
+// Importar Turbo (Rails 7)
 import "@hotwired/turbo-rails"
-import "./controllers"
+
+// Importar Bootstrap e Popper.js
 import * as bootstrap from "bootstrap"
+import { Tooltip, Toast, Popover } from "bootstrap"
+
+// Importar jQuery
+import $ from "jquery"
+
+// Importar AdminLTE
+import "admin-lte"
+
+// Importar seus controladores Stimulus
+import "./controllers"
+
+// Configuração do jQuery (exemplo)
+$(document).ready(function () {
+    console.log('jQuery is ready!');
+});
+
+// Iniciar AdminLTE (opcional, se necessário)
+$(function () {
+    $('[data-widget="pushmenu"]').PushMenu()
+})
