4cb0f9c096ea4424981d65f208107ea9e46fc77a
luis-henrique-carvalho <luisdev9225@gmail.com>
2024-05-23 01:16:05 -0300

    Add schema information to models

--- spec/models/position_spec.rb
+++ spec/models/position_spec.rb
@@ -1,3 +1,29 @@
+# == Schema Information
+#
+# Table name: positions
+#
+#  id          :bigint           not null, primary key
+#  carrer      :integer
+#  city        :string
+#  contract    :integer
+#  description :text
+#  name        :string
+#  publish     :boolean
+#  remote      :boolean
+#  state       :string
+#  summary     :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  company_id  :bigint           not null
+#
+# Indexes
+#
+#  index_positions_on_company_id  (company_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (company_id => companies.id)
+#
 require 'rails_helper'
 
 RSpec.describe Position, type: :model do
