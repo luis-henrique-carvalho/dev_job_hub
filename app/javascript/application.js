b895f16f43b550e0fc0cc3435a9d2ba98fbe7aa8
luis-henrique-carvalho <luisdev9225@gmail.com>
2024-05-28 02:22:11 -0300

    Update application.js and header.html.erb files

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,23 +1,7 @@
 // Importar Turbo (Rails 7)
 import "@hotwired/turbo-rails"
 import "../../vendor/assets/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"
-
-// Importar Bootstrap e Popper.js
-import * as bootstrap from "bootstrap"
-import { Tooltip, Toast, Popover } from "bootstrap"
-
-// Importar jQuery
-import $ from "jquery"
+import "../../vendor/assets/adminlte/build/js/AdminLTE.js"
 
 // Importar seus controladores Stimulus
 import "./controllers"
-
-// Configuração do jQuery (exemplo)
-$(document).ready(function () {
-    console.log('jQuery is ready!');
-});
-
-// Iniciar AdminLTE (opcional, se necessário)
-$(function () {
-    $('[data-widget="pushmenu"]').PushMenu()
-})
